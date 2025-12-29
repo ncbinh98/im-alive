55b1e59bd38841ec91b07169e3c1e53fc21d8f34
Blue Ace <deadtoy0101@gmail.com>
2025-12-29 14:59:22 +0700

    learning(alive-11): leaky bucket rate limit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,13 +12,15 @@ import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { Public } from './decorators/auth.decorator';
 import { TokenBucketRatelimiterInterceptor } from 'src/rate-limters/token-bucket.rate-limiter';
+import { LeakyBucketRatelimiterInterceptor } from 'src/rate-limters/leaky-bucket.rate-limiter';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authSvc: AuthService) {}
 
   @Post('/login')
-  @UseInterceptors(TokenBucketRatelimiterInterceptor)
+  // @UseInterceptors(TokenBucketRatelimiterInterceptor)
+  @UseInterceptors(LeakyBucketRatelimiterInterceptor)
   login(@Body() dto: LoginDto) {
     return this.authSvc.login(dto);
   }
